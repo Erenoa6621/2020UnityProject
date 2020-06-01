7242f239815433c37c466bce2423e6326cd69f57
ERENOA <m011823932@edu.teu.ac.jp>
2020-06-01 10:16:51 +0900

    これから俺たちの冒険の始まりだ

---
+++ 2020Unity/Assets/CharacterEditorPackage/Code/Game/LevelManagement/Goal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    public Text goalText;
+    public Button retryButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            goalText.gameObject.SetActive(true);
+            retryButton.gameObject.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}

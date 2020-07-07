f13b8d650266bab93832ada5a33f1128db1a3ff3
ERENOA <m011823932@edu.teu.ac.jp>
2020-07-07 13:49:31 +0900

    drtf

---
+++ 2020Unity/Assets/Shot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bullet;
+    private Vector3 pos;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pos = new Vector3(Random.Range(10.0f,-10.0f), Random.Range(10.0f, -10.0f), 0f);
+            GameObject trueBullet = Instantiate(bullet, pos, Quaternion.identity);
+            trueBullet.transform.localScale = new Vector3(1.0f, 1.0f, 5.0f);
+        }
+        
+    }
+}

c8a6fd3f45ab5871db3d161d988138b1bd5007f1
ERENOA <m011823932@edu.teu.ac.jp>
2020-08-04 21:17:23 +0900

    でけた

---
+++ 2020Unity/Assets/Scrips/ScoreMane.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreMane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int score = 0;
+    int baseScore = 10;
+    int combo = 0;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = "Score : " + score;
+    }
+
+    public void AddScore()
+    {
+        combo++;
+        score += combo * baseScore;
+    }
+    public void ResetCombo()
+    {
+        combo = 0;
+    }
+
+}
